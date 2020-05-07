e6f9ff36199bb6d3a38994347aec3afa4ad2aabf
Cristopher Alvear Candia <caalvearc@achs.cl>
2020-05-07 18:44:44 -0400

    feat: eslint added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>This also works!</Text>
-    </View>
-  );
+export default function App()
+{
+    return (
+        <View style={ styles.container }>
+            <Text>This also works and works!</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
