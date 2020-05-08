a5b8f464eb2bba2853db9ac54b2309d7009b36ad
Cristopher Alvear Candia <caalvearc@achs.cl>
2020-05-08 19:22:40 -0400

    fix: fx

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { shallowEqual, useSelector, useDispatch } from 'react-redux';
-import { TestAction } from '../store/actions';
 import sample from '../store/actions/sample';
 
 export default function Test()
 {
     const dispatch = useDispatch();
     const asdads = useSelector(
-        store => store[TestAction.Key],
+        store => store[sample.Key],
         shallowEqual
     );
 
