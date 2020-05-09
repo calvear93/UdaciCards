f8ae9499647517dc51d7d1eb7d5fc531ad9befed
Cristopher Alvear Candia <caalvearc@achs.cl>
2020-05-09 15:27:21 -0400

    fix: react navigation fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Root } from 'native-base';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { NavigationContainer } from '@react-navigation/native';
 import { store, persistor } from './store/store';
 import Router from './Router';
 
@@ -17,9 +18,11 @@ export default function App() : React.ReactElement
     return (
         <Provider store={ store }>
             <PersistGate persistor={ persistor }>
-                <Root>
-                    <Router />
-                </Root>
+                <NavigationContainer>
+                    <Root>
+                        <Router />
+                    </Root>
+                </NavigationContainer>
             </PersistGate>
         </Provider>
     );
