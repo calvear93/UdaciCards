dac60d32916cb455ecea3bae6c893361f4735146
Cristopher Alvear Candia <caalvearc@achs.cl>
2020-05-08 21:05:31 -0400

    feat: twks

--- src/store/middleware/saga.ts
+++ src/store/middleware/saga.ts
@@ -3,25 +3,28 @@ import { race, take, delay } from 'redux-saga/effects';
 /**
  * Waits for any action type to occur n times.
  *
- * @param {array} types action types.
+ * @param {Array<any>} types action types.
  * @param {number} times times for wait each action type.
  * @param {number} timeout timeout in milliseconds (0 no timeout).
  *
- * @returns {array} actions results.
+ * @returns {Array<any>} actions results.
  */
-function* waitForEvery(types, times, timeout = 0)
+function* waitForEvery(types: Array<any>, times: number, timeout: number = 0): IterableIterator<any>
 {
     // stores every result of action intercepted.
-    let results = [];
-
+    let results: Array<any> = [];
+    // waits for every dispatch (whether action belongs to types array).
     while (times-- > 0)
     {
-        results.push(yield race(types.reduce((awaiter, actionType) =>
-        {
-            awaiter[actionType.toString()] = take(actionType);
+        results.push(
+            yield race(types.reduce((awaiter, actionType) =>
+            {
+                awaiter[actionType] = take(actionType);
 
-            return awaiter;
-        }, timeout === 0 ? {} : { timeout: delay(timeout) })));
+                return awaiter;
+            },
+            timeout === 0 ? {} : { timeout: delay(timeout) }))
+        );
     }
 
     return results;
