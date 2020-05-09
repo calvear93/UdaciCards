455c830a37a53c64cefabfeafac18a52b2b79c87
Cristopher Alvear Candia <caalvearc@achs.cl>
2020-05-09 09:58:36 -0400

    feat: async storage added

--- src/store/reducers/defaults.ts
+++ src/store/reducers/defaults.ts
@@ -1,3 +1,38 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+const storeData = async () =>
+{
+    try
+    {
+        await AsyncStorage.setItem('@storage_Key', 'stored value');
+    }
+    catch (e)
+    {
+        // saving error
+    }
+};
+
+const getData = async () =>
+{
+    try
+    {
+        const value = await AsyncStorage.getItem('@storage_Key');
+        if (value !== null)
+
+            console.log(value);
+    }
+    catch (e)
+    {
+        // error reading value
+    }
+};
+
+(async () =>
+{
+    await storeData();
+    await getData();
+})();
+
 export const SampleDefaults = {
     data: 'ok'
 };
